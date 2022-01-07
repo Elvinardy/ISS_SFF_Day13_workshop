3a310c03d73ab9f481b912e6696cbdf52bee3a2c
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-01-07 13:58:10 +0800

    first commit

---
+++ src/main/java/nus/iss/workshop_day13/Workshop_day13_Springboot/WorkshopDay13SpringbootApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.workshop_day13.Workshop_day13_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDay13SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopDay13SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/workshop_day13/Workshop_day13_Springboot/WorkshopDay13SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.workshop_day13.Workshop_day13_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDay13SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
